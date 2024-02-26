4c4f9f5c320441486fb6870f3dbeb1ddf66f1d19
OperationRecon <mohammadnaas203@gmail.com>
2024-02-26 17:39:32 +0200

    Added the bare-bones of an API server, including router, server configurations and json coder/de-coder.

---
+++ cmd/api/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() *httprouter.Router {
+	// Initialize a new httprouter router instance.
+	router := httprouter.New()
+
+	// Register the relevant methods, URL patterns and handler functions for our
+	// endpoints using the HandlerFunc() method.
+
+	// Set custom handlers for aftermentioned routes
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	// Return the httprouter instance.
+	return router
+}
